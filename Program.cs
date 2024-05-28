65fa56bb52aed2208a54abce7c1bc487ec441a75
Rabogi <xkrules2002@mail.ru>
2024-05-28 22:06:33 +0300

    get method test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+using System.Net;
+
+class Program
+{
+    
+    static int Main()
+    {   
+        string serverhost = "10.147.17.103";
+        string serverport = "5141";
+        // var options = new RestClientOptions(serverhost + ":" + serverport);
+        // var client = new RestClient();
+        // var request = new RestRequest("weatherforecast");
+
+        var url = serverhost + ":" + serverport + "/weatherforecast";
+
+        var response = "";
+
+        using (var wb = new WebClient())
+        {
+           response = wb.DownloadString("http://10.147.17.103:5141/weatherforecast");
+        }
+
+        
+
+        Console.WriteLine(response);
+
+        return 0;
+    }
+}
+

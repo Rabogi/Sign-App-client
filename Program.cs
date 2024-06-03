e36421850d91f9efc8b105adaa1afff3eaa9c629
Rabogi <xkrules2002@mail.ru>
2024-06-03 20:16:53 +0300

    Reading Json

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using Sign_App_server;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Sign_App_client;
 
@@ -8,16 +11,20 @@ class Program
     static string serverport = "5141";
     static async Task<int> Main()
     {   
-        HttpHandler handler = new HttpHandler(serverhost,serverport,false);
-        // Console.WriteLine(await handler.GetString("/weatherforecast"));
-        // Console.WriteLine(await handler.PostString("/hash256","test"));
-        // List<string> list = new List<string>();
-        // list.Add("./files/3v1L0XNoPog.jpg");
-        // list.Add("./files/CWYMDms1cCw.jpg");
-        // Console.WriteLine(await handler.UploadFile("/uploadfiles",list));
-        Console.WriteLine(await handler.DownloadFile("/downloadfiles","3v1L0XNoPog.jpg"));
-
+        // HttpHandler handler = new HttpHandler(serverhost,serverport,false);
+        // // Console.WriteLine(await handler.GetString("/weatherforecast"));
+        // // Console.WriteLine(await handler.PostString("/hash256","test"));
+        // // List<string> list = new List<string>();
+        // // list.Add("./files/3v1L0XNoPog.jpg");
+        // // list.Add("./files/CWYMDms1cCw.jpg");
+        // // Console.WriteLine(await handler.UploadFile("/uploadfiles",list));
+        // Console.WriteLine(await handler.DownloadFile("/downloadfiles","3v1L0XNoPog.jpg"));
         // Console.WriteLine(SlimShady.Sha256Hash("test"));
+
+        var conf = JsonHandler.ReadJson(File.ReadAllText("./config.json"));
+        foreach(var item in conf){
+            Console.WriteLine(item.ToString());
+        }
 
         return 0;
     }
---
+++ lib/JsonHandler.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Nodes;
+using Flurl.Util;
+
+namespace Sign_App_client;
+
+public abstract class JsonHandler
+{
+    public static Dictionary<string,object> ReadJson(string jsonData){
+        var json = new Dictionary<string,object>();
+        foreach (var item in JsonNode.Parse(jsonData).ToKeyValuePairs()){
+            json.Add(item.Key,item.Value);
+        }
+        return json;
+    }
+
+}
